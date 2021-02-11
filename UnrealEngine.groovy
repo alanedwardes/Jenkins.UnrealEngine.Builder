ceaf9e20c8a8d1c58d0dec7fbe84e5af1ac8359e
Alan Edwardes <alan@alanedwardes.com>
2021-02-11 22:40:39 +0000

    Switches visibility.

--- UnrealEngine.groovy
+++ UnrealEngine.groovy
@@ -3,57 +3,57 @@ class UnrealBuildToolGlobalOptions {
     * The path to unreal engine
     */
     public def enginePath(String enginePath) { this.enginePath = enginePath; return this; }
-    public String enginePath;
+    private String enginePath;
     /**
     * The amount of detail to write to the log
     */
     public def verbose(String verbose) { this.verbose = verbose; return this; }
-    public Boolean verbose;
+    private Boolean verbose;
     /**
     * The amount of detail to write to the log
     */
     public def veryVerbose(String verbose) { this.veryVerbose = veryVerbose; return this; }
-    public Boolean veryVerbose;
+    private Boolean veryVerbose;
     /**
     * Disables Perforce functionality (default if not run on a build machine)
     */
     public def noPerforce(String noPerforce) { this.noPerforce = noPerforce; return this; }
-    public Boolean noPerforce;
+    private Boolean noPerforce;
     /**
     * Specifies the path to a log file to write. Note that the default mode (eg. building, generating project files) will create a log file by default if this not specified.
     */
     public def logFileName(String logFileName) { this.logFileName = logFileName; return this; }
-    public String logFileName;
+    private String logFileName;
     /**
     * Whether to include timestamps in the log
     */
     public def logTimestamps(String logTimestamps) { this.logTimestamps = logTimestamps; return this; }
-    public Boolean logTimestamps;
+    private Boolean logTimestamps;
     /**
     * Whether to format messages in MsBuild format
     */
     public def logFromMsBuild(String logFromMsBuild) { this.logFromMsBuild = logFromMsBuild; return this; }
-    public Boolean logFromMsBuild;
+    private Boolean logFromMsBuild;
     /**
     * Whether to write progress markup in a format that can be parsed by other programs
     */
     public def writeProgressMarkup(String writeProgressMarkup) { this.writeProgressMarkup = writeProgressMarkup; return this; }
-    public Boolean writeProgressMarkup;
+    private Boolean writeProgressMarkup;
     /**
     * Whether to ignore the mutex
     */
     public def noMutex(String noMutex) { this.noMutex = noMutex; return this; }
-    public Boolean noMutex;
+    private Boolean noMutex;
     /**
     * Whether to wait for the mutex rather than aborting immediately
     */
     public def waitMutex(String waitMutex) { this.waitMutex = waitMutex; return this; }
-    public Boolean waitMutex;
+    private Boolean waitMutex;
     /**
     * Whether to wait for the mutex rather than aborting immediately
     */
     public def remoteIni(String remoteIni) { this.remoteIni = remoteIni; return this; }
-    public String remoteIni;
+    private String remoteIni;
 
     /**
     * Add the parameters to the specified list.
@@ -82,73 +82,73 @@ class UnrealBuildCookRunParameters extends UnrealBuildToolGlobalOptions {
        * DebugGame
     */
     public def clientConfig(String clientConfig) { this.clientConfig = clientConfig; return this; }
-    public String clientConfig;
+    private String clientConfig;
     /**
     * Sets platforms to build for non-dedicated servers
     */
     public def targetPlatform(String targetPlatform) { this.targetPlatform = targetPlatform; return this; }
-    public String targetPlatform;
+    private String targetPlatform;
     /**
     * Package the project for the target platform
     */
     public def project(String project) { this.project = project; return this; }
-    public String project;
+    private String project;
 
     public def scriptsForProject(String project) { this.scriptsForProject = scriptsForProject; return this; }
-    public String scriptsForProject;
+    private String scriptsForProject;
     /**
     * Directory to archive the client to
     */
     public def archiveDirectory(String project) { this.archiveDirectory = archiveDirectory; return this; }
-    public String archiveDirectory;
+    private String archiveDirectory;
     /**
     * Determines if the build is going to use cooked data
     */
     public def shouldCook(String project) { this.shouldCook = shouldCook; return this; }
-    public Boolean shouldCook;
+    private Boolean shouldCook;
     /**
     * Put this build in a stage directory
     */
     public def shouldStage(String project) { this.shouldStage = shouldStage; return this; }
-    public Boolean shouldStage;
+    private Boolean shouldStage;
     /**
     * Put this build in an archive directory
     */
     public def shouldArchive(String shouldArchive) { this.shouldArchive = shouldArchive; return this; }
-    public Boolean shouldArchive;
+    private Boolean shouldArchive;
     /**
     * Package for distribution of the project
     */
     public def forDistribution(String forDistribution) { this.forDistribution = forDistribution; return this; }
-    public Boolean forDistribution;
+    private Boolean forDistribution;
     /**
     * If build step should be executed
     */
     public def shouldBuild(String shouldBuild) { this.shouldBuild = shouldBuild; return this; }
-    public Boolean shouldBuild;
+    private Boolean shouldBuild;
     /**
     * Package the project for the target platform
     */
     public def shouldPackage(String shouldPackage) { this.shouldPackage = shouldPackage; return this; }
-    public Boolean shouldPackage;
+    private Boolean shouldPackage;
     /**
     * Skips content under /Engine/Editor when cooking
     */
     public def skipCookingEditorContent(String skipCookingEditorContent) { this.skipCookingEditorContent = skipCookingEditorContent; return this; }
-    public Boolean skipCookingEditorContent;
+    private Boolean skipCookingEditorContent;
     /**
     * Should use packfiles
     */
     public def usePak(String skipCookingEditorContent) { this.usePak = usePak; return this; }
-    public Boolean usePak;
+    private Boolean usePak;
     /**
     * The Unreal Engine 4 executable to use
     */
     public def executable(String skipCookingEditorContent) { this.executable = executable; return this; }
-    public String executable;
+    private String executable;
 
     public def noCompileEditor(String noCompileEditor) { this.noCompileEditor = noCompileEditor; return this; }
-    public String noCompileEditor;
+    private String noCompileEditor;
 
     /**
     * Add the parameters to the specified list.
