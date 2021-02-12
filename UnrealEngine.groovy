5e15fc445dd197925d29dfc854eb2eb3eecbffad
Alan Edwardes <alan@alanedwardes.com>
2021-02-12 18:14:30 +0000

    Renames parameter.

--- UnrealEngine.groovy
+++ UnrealEngine.groovy
@@ -179,18 +179,18 @@ class UnrealBuildCookRunParameters extends UnrealBuildToolGlobalOptions {
     /**
     * Run the command.
     */
-    public def UnrealBuildCookRunResult run(WorkflowScript script) {
-        String buildScript = script.isUnix() ? '/Engine/Build/BatchFiles/RunUAT.sh' : '\\Engine\\Build\\BatchFiles\\RunUAT.bat';
+    public def UnrealBuildCookRunResult run(WorkflowScript context) {
+        String buildScript = context.isUnix() ? '/Engine/Build/BatchFiles/RunUAT.sh' : '\\Engine\\Build\\BatchFiles\\RunUAT.bat';
 
         List<String> parameters = [];
         this.addParameters(parameters);
 
         String command = this.enginePath + buildScript + ' BuildCookRun ' + parameters.join(' ');
 
-        if (script.isUnix()) {
-            script.sh(command);
+        if (context.isUnix()) {
+            context.sh(command);
         } else {
-            script.bat(command);
+            context.bat(command);
         }
 
         String noEditorPath = (this.targetPlatform.startsWith('Win') ? 'Windows' : this.targetPlatform) + 'NoEditor';
